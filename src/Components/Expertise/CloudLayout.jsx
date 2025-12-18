bc0498f7b885febb5283266b41e0f46fd9771a26
Saleemahmh <ssaleemah24@gmail.com>
2025-12-18 12:21:25 +0530

    Bugs

--- src/Components/Expertise/CloudLayout.jsx
+++ src/Components/Expertise/CloudLayout.jsx
@@ -1,63 +1,3 @@
-/* import React from 'react'
-import {motion} from "framer-motion";
-
-const CloudLayout = ({children}) => {
-  return (
-    <motion.div
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    exit={{opacity:0}}
-    transition={{duration:0.6}}
-    className='container relative min-h-screen overflow-hidden'>
-        <div className="absolute inset-0 bg-[url('/Skyclouds.jpg')] bg-cover bg-no-repeat bg-center opacity-80 pointer-events-none"></div>
-
-
-
-      <motion.div
-        className="absolute inset-0 bg-[url('/src/assets/driftingcloud_1.png')] bg-contain bg-repeat-x opacity-60 pointer-events-none"
-        animate={{ x: ["0%", "100%"] }}
-        transition={{
-          duration: 55,
-          ease: "linear",
-          repeat: Infinity,
-        }}
-      />
-      
-
- 
-      <motion.div
-        className="absolute inset-0 bg-[url('/src/assets/driftingcloud_3.png')] bg-contain bg-repeat-x opacity-40 pointer-events-none"
-        animate={{ x: ["0%", "-100%"] }}
-        transition={{
-          duration: 80,
-          ease: "linear",
-          repeat: Infinity,
-        }}
-      />
-      <motion.div
-        className="absolute inset-0 bg-[url('/src/assets/driftingcloud_2.png')] bg-cover bg-center opacity-30 blur-xl pointer-events-none"
-        animate={{ x: ["-10%", "10%", "-10%"] }}
-        transition={{
-          duration: 25,
-          ease: "easeInOut",
-          repeat: Infinity,
-        }}
-      />
-       <div className="absolute left-0 bottom-[0px] w-full h-[250px] pointer-events-none overflow-hidden">
-        <motion.div
-          className="absolute left-0 top-0 w-[200%] h-full bg-[url('/src/assets/driftingcloud_4.png')] bg-cover bg-center opacity-30 blur-xl"
-          animate={{ x: ["-10%", "10%", "-10%"] }}
-          transition={{ duration: 25, ease: "easeInOut", repeat: Infinity }}
-        />
-      </div>
-         <div className="relative z-20">
-        {children}
-      </div>
-    </motion.div>
-  )
-}
-
-export default CloudLayout */
 
 import React from 'react'
 import { motion } from "framer-motion";
@@ -69,7 +9,7 @@ const CloudLayout = ({ children }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.6 }}
-      className="relative min-h-screen overflow-hidden"
+      className="relative min-h-screen overflow-x-hidden"
     >
       {/* STATIC SKY BACKGROUND */}
       <div className="absolute inset-0 bg-[url('/Skyclouds.jpg')] bg-cover bg-no-repeat bg-center opacity-80 pointer-events-none"></div>
